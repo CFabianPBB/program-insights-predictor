ecec4ba4e2969405e7fbd8f3c5449faeacc0db9c
CFabianPBB <chris.fabian@tylertech.com>
2025-02-18 14:41:36 -0700

    updated next config ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
