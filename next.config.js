c1665e777ce95f94a15e31bd7dd5feb4c67fe3d7
CFabianPBB <chris.fabian@tylertech.com>
2025-02-20 17:05:19 -0700

    next.config.js changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ const nextConfig = {
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true
+  },
+  distDir: '.next',
+  images: {
+    unoptimized: true
+  },
+  // Make sure static files are exported properly
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/_next' : '',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/_next' : ''
   }
 };
 
